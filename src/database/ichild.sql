b08b2238dfb044b6e58cc1316fcc14fce9075c9e
Murilo Analiel Machado <muriloam@outlook.com>
2023-04-19 20:21:04 -0300

    [add]: start database configuration

---
+++ src/database/ichild.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE Responsaveis (
+    id INT PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    cidade VARCHAR(255) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    cep VARCHAR(20) NOT NULL,
+);
