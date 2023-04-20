15b94b3d1ed0c035bf565a0393a1ee67f5f5aab2
Murilo Analiel Machado <muriloam@outlook.com>
2023-04-20 00:09:06 -0300

    [fix]: button paths

--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -93,7 +93,7 @@
             <p class="banner__title">Os pais aprovam e acompanham.</p>
           </div>
           <div class="main__banner-btn">
-            <a href="./pages/register.html" class="banner__registerBtn">
+            <a href="./pages/register/register.php" class="banner__registerBtn">
               <span>Cadastrar-se</span>
             </a>
             <a href="#about" class="banner__aboutBtn">
