5817ecc76b7b88d91836c778857e9c99321de417
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-01 11:36:51 -0300

    [add]: nav profile info

--- src/utils/navbar-menu.js
+++ src/utils/navbar-menu.js
@@ -2,7 +2,8 @@ let navbarMenuIcon = document.querySelector("#app__nav-icon");
 let navbarMenuClose = document.querySelector(".menu__bar-icon");
 let navbarMenu = document.querySelector(".aside__bar");
 let profileIcon = document.querySelector(".profile-img");
-let profileNav = document.querySelector(".nav__profile");
+let profileNav = document.querySelector(".aside__bar-profile");
+let profileMenuClose = document.querySelector(".profile__info-icon");
 
 navbarMenuIcon.onclick = () => {
   navbarMenu.classList.toggle("openNavbar");
@@ -14,4 +15,22 @@ navbarMenuClose.onclick = () => {
 
 profileIcon.onclick = () => {
   profileNav.classList.toggle("openNavbar");
+};
+
+profileMenuClose.onclick = () => {
+  profileNav.classList.toggle("openNavbar");
+};
+
+document.onclick = (event) => {
+  if (
+    navbarMenu.contains(event.target) ||
+    navbarMenuIcon.contains(event.target) ||
+    profileNav.contains(event.target) ||
+    profileIcon.contains(event.target)
+  ) {
+    return;
+  }
+
+  navbarMenu.classList.remove("openNavbar");
+  profileNav.classList.remove("openNavbar");
 };
