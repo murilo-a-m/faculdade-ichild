9c0f0e1773edad4d98a32d9eb166fbc1292dfe14
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-02 22:30:24 -0300

    [add]: dependent update

---
+++ src/pages/dependentEdit/Dependentupdate.js
@@ -0,0 +1,41 @@
+function removeDisable(input) {
+  input.removeAttribute("disabled");
+}
+
+const btnEdit = document.querySelector("#btnEdit");
+const btnCancel = document.querySelector("#btnCancel");
+const btnSave = document.querySelector("#btnSave");
+
+const inputs = [
+  (inputName = document.querySelector("#infoName")),
+  (inputLastName = document.querySelector("#infoLastname")),
+  (inputCep = document.querySelector("#infoCep")),
+  (inputState = document.querySelector("#infoState")),
+  (inputCity = document.querySelector("#infoCity")),
+  (inputStreet = document.querySelector("#infoStreet")),
+  (inputNumber = document.querySelector("#infoNumber")),
+];
+
+btnEdit.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  btnEdit.classList.add("form-disabled");
+  btnCancel.classList.remove("form-disabled");
+  btnSave.classList.remove("form-disabled");
+
+  inputs.forEach((element) => {
+    element.removeAttribute("disabled");
+    element.classList.remove("disabled");
+  });
+});
+
+btnCancel.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  btnEdit.classList.remove("form-disabled");
+  btnCancel.classList.add("form-disabled");
+  btnSave.classList.add("form-disabled");
+
+  inputs.forEach((element) => {
+    element.setAttribute("disabled", "disabled");
+    element.classList.add("disabled");
+  });
+});
