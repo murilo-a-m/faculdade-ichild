c31dcb7b26275a74514a7243af76b9033718d8a0
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-10 16:36:17 -0300

    [update]: regex cep

--- src/pages/responsible/update.js
+++ src/pages/responsible/update.js
@@ -1,3 +1,41 @@
+function findCep() {
+  const cep = document.querySelector("#infoCep").value;
+  const cepError = document.querySelector("#cep-error");
+
+  if (cep !== "") {
+    let url = "https://brasilapi.com.br/api/cep/v1/" + cep;
+
+    let request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send();
+
+    request.onload = function () {
+      if (request.status === 200) {
+        cepError.textContent = "";
+        let address = JSON.parse(request.response);
+        document.querySelector("#infoState").value = address.state;
+        document.querySelector("#infoCity").value = address.city;
+        if (address.street === undefined) {
+          document.querySelector("#infoStreet").value = "";
+          return true;
+        } else {
+          document.querySelector("#infoStreet").value = address.street;
+          return true;
+        }
+      } else if (request.status === 404) {
+        cepError.textContent = "CEP inválido";
+        return false;
+      } else {
+        alert("Erro ao fazer a requisição");
+        return false;
+      }
+    };
+  } else {
+    cepError.textContent = "Campo obrigatório*";
+    return false;
+  }
+}
+
 const btnEdit = document.querySelector("#btnEdit");
 const btnDelete = document.querySelector("#btnDelete");
 const btnCancel = document.querySelector("#btnCancel");
@@ -37,4 +75,11 @@ btnCancel.addEventListener("click", (ev) => {
     element.setAttribute("disabled", "disabled");
     element.classList.add("disabled");
   });
+});
+
+const cepInput = document.querySelector("#infoCep");
+
+cepInput.addEventListener("blur", () => {
+  console.log("Sss");
+  findCep();
 });
