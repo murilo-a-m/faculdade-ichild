c2c191e51b67d428d0f5539cc907a568af125bcc
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-05-22 12:56:18 -0300

    [add] dependentdelete

--- src/pages/deleteDependent/deleteDependent.js
+++ src/pages/deleteDependent/deleteDependent.js
@@ -1,19 +1,23 @@
 function confirmarExclusaoDependent() {
-    Swal.fire({
-        title: "Você tem certeza?",
-        text: "Você não podera reverter isso!",
+    swal({
+        title: "Tem certeza?",
+        text: "Esta ação não pode ser desfeita!",
         icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Sim, desejo deletar!",
+        buttons: {
+            cancel: "Cancelar",
+            confirm: {
+                text: "Confirmar",
+                value: true,
+                className: "btn-danger"
+            }
+        }
     }).then((result) => {
         if (result.isConfirmed) {
-        document.getElementById("").submit();
+            document.getElementById("#botaoDependentDelete").submit()
         }
     });
-    }
+}
 
-    const button = document.querySelector("#botaoDependentDelete");
+const button = document.querySelector("#botaoDependentDelete");
 
-    button.addEventListener("click", confirmarExclusaoDependent);
+button.addEventListener("click", confirmarExclusaoDependent);
