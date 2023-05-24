5169ec6a7fea3c632f8c14a8f1f4fa5793cc1517
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-24 16:11:52 -0300

    [add]: date on real time

--- src/pages/responsibleMenu/date.js
+++ src/pages/responsibleMenu/date.js
@@ -1,4 +1,16 @@
 const clock = document.querySelector("#clock");
-const date = new Intl.DateTimeFormat("pt-BR").format(new Date());
 
-clock.textContent = date;
+function updateClock() {
+  const date = new Intl.DateTimeFormat("pt-BR").format(new Date());
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+
+  const formattedHours = String(hours).padStart(2, "0");
+  const formattedMinutes = String(minutes).padStart(2, "0");
+
+  clock.textContent = formattedHours + ":" + formattedMinutes + " - " + date;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
