83a2f4d1340b1bd7e7dde9a66ed9849f2ae2228c
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-28 15:47:56 -0300

    [update]: fix bugs

--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -11,10 +11,10 @@
 
     <!-- Bootstrap links -->
     <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
-      crossorigin="anonymous"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+    crossorigin="anonymous"
     />
 
     <!-- Favicon link -->
@@ -247,7 +247,7 @@
     ></script>
 
     <!-- Script AOS -->
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.js"></script>
     <script>
       AOS.init();
     </script>
