6e8669266ba49d98ccbfbcfdced0a3b4b7e01bd2
Murilo Analiel Machado <muriloam@outlook.com>
2023-05-28 23:25:46 -0300

    [update]: updateLog screen to load dependents from tranportes id

--- src/pages/painelLog/loadDependents.php
+++ src/pages/painelLog/loadDependents.php
@@ -4,22 +4,23 @@
 
   $sql = "SELECT id, nome
           FROM ichild.Dependentes
-          WHERE id =$transportadorId";
+          WHERE transportadorId =$transportadorId";
 
   if ($result = mysqli_query($conn, $sql)) {
-    if (mysqli_num_rows($result) > 0) {
 
-      echo '<div class="col-md-8 mt-2">';
-      echo '<label for="inputTurn" class="form-label">Dependente</label>';
-      echo '<select id="inputTurn" class="form-select" name="turno">';
+    echo '<div class="col-md-8 mt-2">';
+    echo '<label for="inputTurn" class="form-label">Dependente</label>';
+    echo '<select id="inputTurn" class="form-select" name="turno">';
+
+    if (mysqli_num_rows($result) > 0) {
 
       while ($row = mysqli_fetch_assoc($result)){
         echo '<option value="'.$row['id'].'">'.$row['nome'].'</option>';
       }
-
-      echo '</select>';
-      echo '<span id="turn-error" class="error"></span>';
-      echo '</div>';
     }
+    
+    echo '</select>';
+    echo '<span id="turn-error" class="error"></span>';
+    echo '</div>';
   }   
 ;?>
