6a2824d52470309a428887c5e2fd9d0f50261be2
Murilo Analiel Machado <muriloam@outlook.com>
2023-05-30 18:44:50 -0300

    [add]

--- src/pages/painelLog/painelLogExe.php
+++ src/pages/painelLog/painelLogExe.php
@@ -1,28 +1,31 @@
 <html>
     <?php
         require_once '../../database/connection.php';
+        $transportadorId = $_SESSION['id'];
 
-        $responsavelId = $_SESSION['id'];
-        // $transportadorId = $_SESSION['id'];
-
-        $sql = "SELECT horario, statusLog, localLog, dependentId
+        $sql = "SELECT Log_do_dia.horario, Log_do_dia.statusLog, Log_do_dia.localLog, Log_do_dia.dependentId, Dependentes.nome
                 FROM Log_do_dia
-                WHERE transportadorId = $transportadorId";
+                INNER JOIN Dependentes ON Log_do_dia.dependentId = Dependentes.id
+                WHERE Log_do_dia.transportadorId = $transportadorId";
 
         if ($result = mysqli_query($conn, $sql)) {
             if (mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo "<div class='dependenteLog'>";
+
+                    if ($row['statusLog'] == 'Chegou ao destino') {
                         echo "<div class='iconLogTransport'>";
                         echo "</div>";
-                        echo "<div class = iconLogTransport2";
+                    } else {
+                        echo "<div class='iconLogTransport2'>";
                         echo "</div>";
-                        echo "<div class='dadosLog'>";
-                        echo "<h6 class='infoDadosLog'>" . $row['horario'] . "</h6>";
-                        echo "<h6 class='infoDadosLog'>" .  "Elvis</h6>";
-                        echo "<h6 class='infoDadosLog'>" . $row['statusLog'] . "</h6>";
-                        echo "<h6 class='infoDadosLog'>" . $row['localLog'] . "</h6>";
-                        echo "</div>";
+                    }
+                    echo "<div class='dadosLog'>";
+                    echo "<h6 class='infoDadosLog'>" . date('d/m/Y H:i:s', strtotime($row['horario'])) . "</h6>";
+                    echo "<h6 class='infoDadosLog'>" . $row['nome'] . "</h6>";
+                    echo "<h6 class='infoDadosLog'>" . $row['statusLog'] . "</h6>";
+                    echo "<h6 class='infoDadosLog'>" . $row['localLog'] . "</h6>";
+                    echo "</div>";
                     echo "</div>";
                 }
             }
