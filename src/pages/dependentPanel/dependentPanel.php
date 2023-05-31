5992e85086445ee8fe5453bee6f9dc98526c6909
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-31 20:40:58 -0300

    [update]: search style

--- src/pages/dependentPanel/dependentPanel.php
+++ src/pages/dependentPanel/dependentPanel.php
@@ -56,7 +56,7 @@
           <p class="card__title">Dependentes</p>
           <div class="search-content">
             <input type="search" class="form-control w-100" placeholder="Pesquisar" id="search">
-            <button class="btn btn-style" id="button">
+            <button class="btn btn-styleResponsible" id="button">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
               </svg>
@@ -65,7 +65,7 @@
         </div>
         <div class="table-responsive">
           <table class="table table-striped text-center table__panel">
-            <thead class="table-dark">
+            <thead class="table-light" id="tableResponsible">
               <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Nome</th>
