045b30016ae013f10194a42f3df6936a10d72f70
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-05-31 19:29:47 -0300

    [fix-bugs] notify

--- src/pages/dependendNotify/dependendNotifyExe.php
+++ src/pages/dependendNotify/dependendNotifyExe.php
@@ -1,6 +1,7 @@
 <?php
+    session_start();
     require_once "../../database/connection.php";
-    header('Content-Type: application/json');
+
 
     $transportadorId = $_SESSION['id'];
     $message = $_POST ['messageNot'];
@@ -11,19 +12,20 @@
     
 
     if ($result = mysqli_query($conn, $sql)) {       
-            $status = 'success';
-            $message = 'Requisição bem sucedida';
-            $statusCode = 200;
+        $status = 'success';
+        $message = 'Requisição bem sucedida';
+        $statusCode = 200;
 
-            http_response_code($statusCode);
+        http_response_code($statusCode);
 
-            $response = array(
-                'status' => $status,
-                'message' => $message,
-            );
-            $jsonResponse = json_encode($response);
-            echo $jsonResponse;
-            exit;
+        $response = array(
+            'status' => $status,
+            'message' => $message,
+        );
+        $jsonResponse = json_encode($response);
+        echo $jsonResponse;
+        header('location: ./notifyForm.php');
+        exit;
 
     } else {
         $status = 'error';
