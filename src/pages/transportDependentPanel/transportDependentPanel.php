78f4d7468b7f2749498f093b9f69bfa1819cbb13
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-05-31 15:09:50 -0300

    [add]: search bar

--- src/pages/transportDependentPanel/transportDependentPanel.php
+++ src/pages/transportDependentPanel/transportDependentPanel.php
@@ -48,6 +48,14 @@
     
     <main class="container__main-panel">
       <div id="panel" class="panel__content">
+        <div class="panel_search mb-2">
+          <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
+          <button class="btn btn-style">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+          </button>
+        </div>
         <div class="table-responsive">
           <table class="table table-striped text-center table__panel">
             <thead class="table-dark">
