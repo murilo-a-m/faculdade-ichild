0fee98e2fe440cd42559629e39158ca1b8861a8d
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-06-05 11:29:10 -0300

    [update] transport log

--- src/pages/painelLog/painelLogExe.php
+++ src/pages/painelLog/painelLogExe.php
@@ -1,35 +1,88 @@
-<html>
-    <?php
-        require_once '../../database/connection.php';
-        $transportadorId = $_SESSION['id'];
+<?php 
+    require_once '../../database/connection.php';
 
-        $sql = "SELECT Log_do_dia.horario, Log_do_dia.statusLog, Log_do_dia.localLog, Log_do_dia.dependentId, Dependentes.nome
-                FROM Log_do_dia
-                INNER JOIN Dependentes ON Log_do_dia.dependentId = Dependentes.id
-                WHERE Log_do_dia.transportadorId = $transportadorId";
+    $transportadorId = $_SESSION['id'];
 
-        if ($result = mysqli_query($conn, $sql)) {
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<div class='dependenteLog'>";
+    $sql = "SELECT l.id, l.horario, l.statusLog, l.localLog, d.nome AS dependente_nome, d.sobrenome AS dependente_sobrenome
+        FROM ichild.log_do_dia l
+        INNER JOIN ichild.Dependentes d ON l.dependentId = d.id
+        WHERE l.transportadorId = $transportadorId
+        ORDER BY l.horario DESC";
 
-                    if ($row['statusLog'] == 'Chegou ao destino') {
-                        echo "<div class='iconLogTransport'>";
-                        echo "</div>";
-                    } else {
-                        echo "<div class='iconLogTransport2'>";
-                        echo "</div>";
-                    }
-                    echo "<div class='dadosLog'>";
-                    echo "<h6 class='infoDadosLog'>" . date('d/m/Y H:i:s', strtotime($row['horario'])) . "</h6>";
-                    echo "<h6 class='infoDadosLog'>" . $row['nome'] . "</h6>";
-                    echo "<h6 class='infoDadosLog'>" . $row['statusLog'] . "</h6>";
-                    echo "<h6 class='infoDadosLog'>" . $row['localLog'] . "</h6>";
-                    echo "</div>";
-                    echo "</div>";
-                }
+    if(!empty($_GET['search'])) {
+      $data = $_GET['search'];
+      $sql = "SELECT l.id, l.horario, l.statusLog, l.localLog, d.nome AS dependente_nome, d.sobrenome AS dependente_sobrenome
+        FROM ichild.log_do_dia l
+        INNER JOIN ichild.Dependentes d ON l.dependentId = d.id
+        WHERE (l.horario LIKE '%$data%' OR l.statusLog LIKE '%$data%' OR l.localLog LIKE '%$data%' OR d.nome LIKE '%$data%' OR d.sobrenome LIKE '%$data%')
+        AND l.transportadorId = $transportadorId
+        ORDER BY l.horario DESC";
+
+
+      if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)){
+            echo "<tr class='panelContent'>";
+
+            if ($row['statusLog'] == 'Chegou ao destino') {
+              echo "<th scope='row'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#27374D' class='bi bi-circle-fill' viewBox='0 0 16 16'>
+                          <circle cx='8' cy='8' r='8'/>
+                        </svg>
+                    </th>";
+            } else {
+                echo "<th>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#B3B8BB' class='bi bi-circle-fill' viewBox='0 0 16 16'>
+                          <circle cx='8' cy='8' r='8'/>
+                        </svg>
+                    </th>";
             }
+
+            $horario = strtotime($row['horario']);
+            $data = date('Y-m-d', $horario);
+            $hora = date('H:i:s', $horario);
+
+            echo "<td>$data</td>";
+            echo "<td>$hora</td>";
+            echo "<td>$row[statusLog]</td>";
+            echo "<td>$row[localLog]</td>";
+            echo "<td>$row[dependente_nome] $row[dependente_sobrenome]</td>";
+            echo "</tr>";
+          }
         }
-    ?>
-    <script src="./painelLog.js"></script>
-</html>
+      } 
+    } else {
+      if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)){
+            echo "<tr class='panelContent'>";
+            if ($row['statusLog'] == 'Chegou ao destino') {
+              echo "<th scope='row'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#27374D' class='bi bi-circle-fill' viewBox='0 0 16 16'>
+                          <circle cx='8' cy='8' r='8'/>
+                        </svg>
+                    </th>";
+            } else {
+                echo "<th>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#B3B8BB' class='bi bi-circle-fill' viewBox='0 0 16 16'>
+                          <circle cx='8' cy='8' r='8'/>
+                        </svg>
+                    </th>";
+            }
+
+            $horario = strtotime($row['horario']);
+            $data = date('Y-m-d', $horario);
+            $hora = date('H:i:s', $horario);
+
+            echo "<td>$data</td>";
+            echo "<td>$hora</td>";
+            echo "<td>$row[statusLog]</td>";
+            echo "<td>$row[localLog]</td>";
+            echo "<td>$row[dependente_nome] $row[dependente_sobrenome]</td>";
+            echo "</tr>";
+          }
+        }
+      } 
+    }
+;?>
+
