b2b24e0bc575bf8cf8e5ecbc1de1ad2ddf3e7c2b
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-06-06 19:14:25 -0300

    [add] th transportador

--- src/pages/dependendNotify/dependentMsgExib.php
+++ src/pages/dependendNotify/dependentMsgExib.php
@@ -3,12 +3,15 @@
 
     // $transportadorId = $_SESSION['id'];
 
-    $sql = "SELECT d.nome AS dependente_nome, d.sobrenome AS dependente_sobrenome,
+    $sql = "SELECT d.nome AS dependente_nome, r.nome AS responsavel_nome, r.sobrenome AS responsavel_sobrenome, d.sobrenome AS dependente_sobrenome,
     t.nome AS transportador_nome, t.sobrenome AS transportador_sobrenome,
     m.mensagem
     FROM Mensagem m
     INNER JOIN Dependentes d ON m.dependentId = d.id
-    INNER JOIN Transportadores t ON m.transportadorId = t.id";
+    INNER JOIN Transportadores t ON m.transportadorId = t.id
+    INNER JOIN Responsaveis r ON r.id = d.responsavelId ";
+    
+    ;
 
 
 
@@ -17,7 +20,14 @@
             while ($row = mysqli_fetch_assoc($result)){
                 echo "<tr class='panelContent'>";
                 echo "<td>$row[dependente_nome] $row[dependente_sobrenome]</td>";
-                echo "<td>$row[transportador_nome] $row[transportador_sobrenome]</td>";
+
+                if ($_SESSION['role']=='responsavel'){
+                    echo "<td>$row[responsavel_nome] $row[responsavel_sobrenome]</td>";
+                }else{
+                    echo "<td>$row[transportador_nome] $row[transportador_sobrenome]</td>";
+                }
+
+                
                 echo "<td>$row[mensagem]</td>";
                 echo "</tr>";
             }
