9a039b50480033ee7d3770745a35d88bd1b18f20
Murilo Analiel Machado <muriloam@outlook.com>
2023-06-06 18:57:09 -0300

    [update]: create a session timeout to responsible to exit session authorization

--- src/components/transportAuthorization.php
+++ src/components/transportAuthorization.php
@@ -1,5 +1,21 @@
 <?php
-  session_start();
+    session_start();
+
+    $expiracao = 21600;
+    $inatividade_minima = 21600;
+
+    if (isset($_SESSION['ultimo_acesso'])) {
+        $tempo_atual = time();
+        $tempo_inativo = $tempo_atual - $_SESSION['ultimo_acesso'];
+        if ($tempo_inativo >= $inatividade_minima) {
+            session_destroy();
+             header('Location: /ichild/src/pages/login/login.php');
+            exit;
+        }
+    }
+
+    $_SESSION['ultimo_acesso'] = time();
+
   if (!isset($_SESSION['id']) || !$_SESSION['role'] == 'transportador'  ){
     header('location: ../loginTransport/loginTransport.php?erro=true');
     exit;
